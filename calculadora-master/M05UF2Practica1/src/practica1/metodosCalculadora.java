a17c2527449b040de308ed49d74613d13cc57ab6
adaw22kcubias <adaw22kcubias@inslaferreria.cat>
2023-02-06 18:56:55 +0100

    metodos inicializados

---
+++ calculadora-master/M05UF2Practica1/src/practica1/metodosCalculadora.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica1;
+
+/**
+ *
+ * @author Karla Cubias
+ */
+public class metodosCalculadora {
+
+    static int suma(int n1, int n2) {
+        int res;
+        res = n1 + n2;
+        return res;
+    }
+
+    static int resta(int n1, int n2) {
+        int res;
+        res = n1 - n2;
+        return res;
+    }
+
+    static int multiplicacion(int n1, int n2) {
+        int res;
+        res = n1 * n2;
+        return res;
+    }
+
+    static int division(int n1, int n2) {
+        int res;
+       res =  n1 * n2;
+
+        return res;
+    }
+
+}
